b40337d66d42fb2617eaed1848d946d0c3b306a3
Garry Kuwanto <gkuwanto@gmail.com>
2020-08-10 03:09:50 +0700

    change bash script to use variables

--- process_sql.sh
+++ process_sql.sh
@@ -1,5 +1,4 @@
 mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -e "create database $1"
 mysql -u$MYSQL_USER -p$MYSQL_PASSWORD $1 < $2
 rm out.csv
-mysql -ugarry -pgarry gu -e "select * from langlinks where ll_lang='en' into outfile '$(pwd)/out.csv' fields terminated by ',' enclosed by '\"' lines terminated by '\\n';"
-python get_ids.py out.csv
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD $1 -e "select * from langlinks where ll_lang='en' into outfile '$(pwd)/out.csv' fields terminated by ',' enclosed by '\"' lines terminated by '\\n';"
